9171416c5218a9859fbbd2bf86ac4c0efb94d2d3
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-07 23:08:52 +0330

    create quiz

---
+++ app.js
@@ -0,0 +1,81 @@
+const correctAnswer = ['b', 'b', 'b', 'b'];
+const form = document.querySelector('.quiz_form');
+const show = document.querySelector('#show__res');
+const span = document.querySelector('#score');
+const resl = document.querySelector('#score');
+const timer = document.querySelector('.timer');
+const start = document.querySelector('#start');
+const _p = document.querySelector('.container');
+const rest = document.querySelector('#rest');
+
+// window.addEventListener('DOMContentLoaded', e => {
+//     onscroll = () => {
+//         let i = 0;
+//         const conter = setInterval(() => {
+//             timer.innerHTML = `timer${i}`;
+//             timer.classList.remove('hiden');
+//             if (i >= 5) {
+//                 clearInterval(conter);
+//             }
+//             else {
+//                 i++;
+//             }
+//         }, 1000);
+//     }
+// });
+
+
+rest.addEventListener('click', e => {
+    location.reload();
+})
+
+
+let container;
+start.addEventListener('click', e => {
+    let i = 30;
+    timer.classList.remove('hiden');
+    conterTimer = setInterval(() => {
+        timer.innerHTML = `Timer : ${i}`;
+        if (i <= 0) {
+            clearInterval(conterTimer);
+            scrollTo(0, 0);
+            const p = document.createElement('p');
+            p.style.color = 'white';
+            p.style.fontSize = '3.5rem';
+            p.innerHTML = `Time End ....`;
+            _p.appendChild(p);
+        } else {
+            i--
+        }
+      
+    }, 1000);
+    
+});
+    
+    
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    clearInterval(conterTimer);
+    let score = 0;
+    const userAnsewers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value,];
+    userAnsewers.forEach((answer, index) => {
+        if (answer === correctAnswer[index]) {
+            span.innerHTML = score += 25;
+        }
+        show.classList.remove('hiden');
+        scrollTo(0, 0);
+            
+        let conter = 0;
+        const checkout = setInterval(() => {
+            resl.textContent = `${conter}`;
+            if (score == conter) {
+                clearInterval(checkout);
+            }
+            else {
+                conter++;
+            }
+        }, 10);
+    });
+});
+    
+  
