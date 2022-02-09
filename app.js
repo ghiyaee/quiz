a10ad2663be92ed592820fea4ff2018aba6fc764
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-09 18:42:04 +0330

    add qustion

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const correctAnswer = ['b', 'b', 'b', 'b','b','b','b','b'];
+const correctAnswer = ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'];
 const form = document.querySelector('.quiz_form');
 const show = document.querySelector('#show__res');
 const span = document.querySelector('#score');
@@ -55,12 +55,13 @@ start.addEventListener('click', e => {
 form.addEventListener('submit', e => {
     e.preventDefault();
     clearInterval(conterTimer);
+    // debugger
     let score = 0;
     const userAnsewers = [form.q1.value, form.q2.value, form.q3.value,
-    form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value];
+    form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
     userAnsewers.forEach((answer, index) => {
         if (answer === correctAnswer[index]) {
-            span.innerHTML = score += 12.5;
+            span.innerHTML = score +=10;
         }
         show.classList.remove('hiden');
         scrollTo(0, 0); 
